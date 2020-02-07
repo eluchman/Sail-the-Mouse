b009ef49a0829db711848ecf91fba09efa8b8c84
Eric W Luchman <48758468+eluchman@users.noreply.github.com>
2020-02-07 14:59:43 -0500

    Add files via upload
    Updated Directory Component with <Card> Component and Event Handler.

--- DirectoryComponent.js
+++ DirectoryComponent.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Directory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            selectedCruiseship: null
         };
+    }
+
+    onCruiseshipSelect(cruiseship) {
+        this.setState({selectedCruiseship: cruiseship})
+    }
+
+    renderSelectedCruiseship(cruiseship) {
+        if (cruiseship) {
+            return (
+                <Card>
+                    <CardImg top src={cruiseship.image} alt={cruiseship.name} />
+                    <CardBody>
+                        <CardTitle>{cruiseship.name}</CardTitle>
+                        <CardText>{cruiseship.destinations}</CardText>
+                    </CardBody>
+                </Card>
+            )
+
+        }
     }
 
     render() {
         const directory = this.props.cruiseships.map(cruiseship => {
             return (
                 <div key={cruiseship.id} className="col">
-                    <img src={cruiseship.image} alt={cruiseship.name} />
-                    <h2>{cruiseship.name}</h2>
-                    <p>{cruiseship.destinations}</p>
+                   <Card onClick= {() => this.onCruiseshipSelect(cruiseship)}>
+                    <CardImg width="100%" src={cruiseship.image} alt={cruiseship.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{cruiseship.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         });
